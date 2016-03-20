40e17e035b65266e5db3e12023a48792a084969a
davideberra <davide_berra@yahoo.it>
2016-03-20 15:50:29 +0100

    modified:   cpu/z80_regs.h

--- cpu/z80_regs.h
+++ cpu/z80_regs.h
@@ -18,12 +18,12 @@
 */
 
 
-#ifndef Z80_H
-#define Z80_H
+#ifndef Z80_REGS_H
+#define Z80_REGS_H
 
 #include <stdint.h>
 
-/* structs emulating 8080 registers and flags */
+/* structs emulating z80 registers and flags */
 typedef struct z80_flags_s
 {
     uint8_t  cy:1;
@@ -36,77 +36,19 @@ typedef struct z80_flags_s
     uint8_t  s:1;
 } z80_flags_t;
 
-typedef struct z80_state_s
-{
-    uint8_t        spare;
-    uint8_t        a;
+
+/* flags offsets */
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    uint8_t        c;
-    uint8_t        b;
-#else
-    uint8_t        b;
-    uint8_t        c;
+
+    #define FLAG_OFFSET_CY 0
+    #define FLAG_OFFSET_N  1
+    #define FLAG_OFFSET_P  2
+    #define FLAG_OFFSET_U3 3
+    #define FLAG_OFFSET_AC 4
+    #define FLAG_OFFSET_U5 5
+    #define FLAG_OFFSET_Z  6
+    #define FLAG_OFFSET_S  7
+
 #endif
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    uint8_t        e;
-    uint8_t        d;
-#else
-    uint8_t        d;
-    uint8_t        e;
-#endif
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    uint8_t        l;
-    uint8_t        h;
-#else
-    uint8_t        h;
-    uint8_t        l;
-#endif
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    uint8_t        ixl;
-    uint8_t        ixh;
-#else
-    uint8_t        ixh;
-    uint8_t        ixl;
-#endif
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    uint8_t        iyl;
-    uint8_t        iyh;
-#else
-    uint8_t        iyh;
-    uint8_t        iyl;
-#endif
-    uint16_t       sp;
-    uint16_t       pc;
-
-    /* shortcuts */
-    uint16_t       *bc;
-    uint16_t       *de;
-    uint16_t       *hl;
-    uint16_t       *ix;
-    uint16_t       *iy;
-    uint8_t        *f;
-
-    uint8_t        memory[65536];
-    uint32_t       cycles;
-
-    uint32_t       skip_cycle;
-
-    z80_flags_t    flags;
-    uint8_t        int_enable;
-    uint8_t        spare2;
-    uint8_t        spare3;
-
-} z80_state_t;
-
-/* defines    */
-#define Z80_MAX_MEMORY 65536
-
-/* prototypes */
-/*z80_state_t   *z80_init(void);
-int            z80_run(void);
-int            z80_execute(unsigned char code);
-int            z80_disassemble(unsigned char *codebuffer, int pc);
-void           z80_print_state(void);
-*/
 
 #endif
